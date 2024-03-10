3bb17c92b18268277f683894f8b2fa4f5c2e0f08
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-10 13:52:05 -0400

    Still troubleshooting issues with returning data from db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const { Pool } = require('pg');
 
 const PORT = process.env.PORT || 3001;
@@ -34,13 +35,14 @@ app.get('/api/role', (req, res) => {
     console.log('Success');
     });
 
-app.get('/api/employee', (req, res) => {
-    pool.query('SELECT * FROM employee', function (err, {rows}) {
-        res.status(200).json({rows});
+function getEmployees(){
+    pool.query('SELECT first_name AS "First Name", last_name AS "Last Name", manager_id AS "Manager" FROM employee', function (err, {rows}) {
+        console.table(rows);
         });
     console.log('Success');
-    });
-    
+    };
+
+//getEmployees();
 
 //app.delete('/api//:id', (req, res) => {
   //  const id = req.params.id;
@@ -49,6 +51,38 @@ app.get('/api/employee', (req, res) => {
      // });
    // });
 
+   inquirer.prompt([
+    {
+        type: 'list',
+        name: 'activity',
+        message: 'What would you like to do?',
+        choices: ["View all departments", 
+        "View all employees",
+        "View all roles",
+        "Add a department",
+        "Add an employee",
+        "Add a role", 
+        "Update an employee role",
+        "Quit"
+    ]
+    }
+])
+.then(
+  (choice) => {
+     switch (choice.activity){
+        case "View all departments": getEmployees();
+        break;
+        case 'View all employees': console.log('View all employees');
+        case 'View all roles': console.log('View all roles');
+        case 'Add a department': console.log('Add a department');
+        case 'Add a role': console.log('Add a role');
+        case 'Add an employee': console.log('Add an employee');
+        case 'Update an employee role': console.log('Add an employee');
+        case 'Quit': console.log("Thank you for using Employee Tracker!");
+            break;
+        }
+   }
+);
 
 app.use((req, res) => {
   res.status(404).end();
