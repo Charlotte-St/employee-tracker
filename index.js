28782e6c960621592f3cb1095a8bf7907d9191c9
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-10 10:22:15 -0400

    Updated index.js; Added inquirer to package.json; Commented out pool and app listener while testing inquirer implementation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const inquirer = require('inquirer');
-const { Pool } = require('pg');
+//const { Pool } = require('pg');
 
 const PORT = 3001;
 const app = express();
@@ -8,17 +8,17 @@ const app = express();
 app.use(express.urlencoded({exprended: false}));
 app.use(express.json());
 
-const pool = new Pool(
-    {
-        user: 'postgres',
-        password: 'tryto1',
-        host: 'localhost',
-        database: 'employee_tracker_db'
-    },
-    console.log('Connected to the employee_tracker_db database')
-)
+//const pool = new Pool(
+  //  {
+    //    user: 'postgres',
+      //  password: 'tryto1',
+       // host: 'localhost',
+        //database: 'employee_tracker_db'
+    //},
+    //console.log('Connected to the employee_tracker_db database')
+//)
 
-pool.connect();
+//pool.connect();
 
 //Inquirer prompts
 
@@ -38,9 +38,20 @@ inquirer.prompt([
         'Update an employee role']
     }
 ]).then(
+    (choice) => {
+        switch (choice.activity){
+            case 'View all roles': console.log('View all roles');
+            case 'View all employees': console.log('View all employees');
+            case 'Add a department': console.log('Add a department');
+            case 'Add a role': console.log('Add a role');
+            case 'Add an employee': console.log('Add an employee');
+            case 'Update an employee role': console.log('Add an employee');
+        }
+    }
+
 //Add switch case for each choice
 )
 
-app.listener(PORT, () =>{
-    console.log(`Server listening on port ${PORT}`);
-});
+//app.listener(PORT, () =>{
+  //  console.log(`Server listening on port ${PORT}`);
+//});
